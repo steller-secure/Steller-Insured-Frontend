2653e22718efd12d63a2e7511a2a5dcb191f39b6
Abdul <a-ahmed@leadway-group.com>
2025-05-31 23:59:54 +0100

    feat(ui): improve insurance categories with filter, transitions, and responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 
 import Hero from "@/src/components/Hero";
 import Navbar from "@/src/components/Navbar";
+import InsuranceCategories from "../components/InsuranceCategories";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
        <Navbar />
-       <Hero />
+      <Hero />
+      <InsuranceCategories />     
       </div>
   );
 }
